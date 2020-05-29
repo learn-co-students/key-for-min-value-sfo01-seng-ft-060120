a4b22970caf2763ef5fcd2d8a8d7e61ea10d8f91
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-29 20:20:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,24 @@
 
 def key_for_min_value(name_hash)
 
+  # separate the keys
+  keys = []
+  name_hash.each do |k, v|
+    keys.push(k)
+  end
+
+  # initialize "smallest" as first element of keys array
+  smallest_v = name_hash[keys[0]]
+  smallest_k = keys[0]
+
+  # compare values, assign smallest k/v pair when appropriate
+  keys.each do |k|
+    if name_hash[k] < smallest_v
+      smallest_v = name_hash[k]
+      smallest_k = k
+    end
+  end
+
+  # return smallest key
+  smallest_k
 end
